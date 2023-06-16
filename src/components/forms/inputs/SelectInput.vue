6beb342723b1b5f11e225902b5d05859f287f002
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-16 09:14:20 -0400

    custom inputs

---
+++ src/components/forms/inputs/SelectInput.vue
@@ -0,0 +1,31 @@
+<script setup lang='ts'>
+
+export interface Props {
+	modelValue?: string | number
+	required?: boolean
+	id: string
+	label?: string
+	options: Array<{ label: string; value: string | number }>
+}
+
+defineProps<Props>()
+const $emit = defineEmits<{ (e: 'update:modelValue', v: string | number): void }>()
+
+function onChange(event: Event) {
+	const value = (event.target as HTMLSelectElement).value
+	$emit('update:modelValue', value)
+}
+</script>
+
+<template>
+	<div>
+
+		<label :for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+		<select :id="id" @change="onChange" :value="modelValue" :name="id"
+			class="bg-gray-50 border border-primary-300 text-primary-700 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-primary-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+			<option v-for="(option, key) in options" :key="`option-${key}`" :value="option.value">
+				{{ option.label }}
+			</option>
+		</select>
+	</div>
+</template>
--- src/components/forms/inputs/TextInput.vue
+++ src/components/forms/inputs/TextInput.vue
@@ -1,22 +1,29 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 export interface Props {
-  modelValue: string | number
-  type: 'text' | 'number' | 'email' | 'tel' | 'password'
-  required?: boolean
-  id: string
-  label?: string
-  placeholder?: string
+	modelValue?: string | number
+	type: 'text' | 'number' | 'email' | 'tel' | 'password' | 'textarea' | 'currency'
+	required?: boolean
+	id: string
+	label?: string
+	placeholder?: string
+	rows?: number
+	min?: number
+	max?: number
+	step?: number
 }
 
-defineProps<Props>()
+const $props = defineProps<Props>()
 const $emit = defineEmits<{ (e: 'update:modelValue', v: string | number): void }>()
 
+const realType = computed(() => $props.type === 'currency' ? 'number' : $props.type)
 /**
  * validate
  * @param val
  */
 function validate(val: string | number) {
-  return true
+	return true
 }
 
 /**
@@ -24,28 +31,24 @@ function validate(val: string | number) {
  * @param event
  */
 function onChange(event: Event) {
-  const value = (event.target as HTMLInputElement).value
-  if (validate(value)) $emit('update:modelValue', value)
+	let value = (event.target as HTMLInputElement).value
+	if ($props.type === 'currency')
+		value = Number(value).toFixed(2)
+	if (validate(value)) $emit('update:modelValue', value)
 }
 </script>
 
 <template>
-  <div>
-    <label
-      :for="id"
-      class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      v-if="label"
-      >{{ label }}</label
-    >
-    <input
-      @change="onChange"
-      :value="modelValue"
-      :type="type"
-      :name="id"
-      :id="id"
-      :placeholder="placeholder"
-      class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      :required="required"
-    />
-  </div>
+	<div>
+		<label :for="id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" v-if="label">{{ label }}</label>
+
+		<textarea v-if="type === 'textarea'" @change="onChange" :value="modelValue" :name="id" :id="id"
+			:placeholder="placeholder" :rows="rows"
+			class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			:required="required"></textarea>
+
+		<input v-else @change="onChange" :value="modelValue" :type="realType" :name="id" :id="id" :placeholder="placeholder"
+			class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+			:required="required" :min="min" :max="max" :step="step" />
+	</div>
 </template>

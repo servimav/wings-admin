72375322a08837679846529b46a37903c3402fe1
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-17 12:46:22 -0400

    available visualization

--- src/components/widgets/OfferWidget.vue
+++ src/components/widgets/OfferWidget.vue
@@ -13,8 +13,19 @@ defineProps<Props>()
   <div
     class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
   >
-    <div class="p-2 bg-slate-100">
+    <div class="p-2 bg-slate-100 relative">
       <img class="rounded-t-lg" src="../../assets/logo.svg" alt="product image" />
+
+      <div class="absolute bottom-1 right-1 z-10">
+        <div
+          class="rounded-full shadow-md border p-1 text-xs"
+          :class="
+            data.available
+              ? 'bg-green-500 text-white border-green-300'
+              : 'bg-slate-300 text-black border-slate-400'
+          "
+        ></div>
+      </div>
     </div>
     <div class="p-2">
       <h5 class="text-sm text-gray-900 dark:text-white">{{ data.name }}</h5>

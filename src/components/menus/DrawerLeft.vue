b9e93ef04dc92d6b9b93a3effd725093af5f7fd3
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-19 15:27:39 -0400

    remove padding top

--- src/components/menus/DrawerLeft.vue
+++ src/components/menus/DrawerLeft.vue
@@ -82,7 +82,7 @@ function goTo(to?: RouteLocationRaw) {
 <template>
   <aside
     :id="elementId"
-    class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
+    class="fixed top-0 left-0 z-40 w-64 h-screen pt-6 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700"
     aria-label="Sidebar"
   >
     <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">

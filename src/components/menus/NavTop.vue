c82698e62bc696d9b9391efa73396d7d63704e67
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-20 12:31:11 -0400

    add icons

--- src/components/menus/NavTop.vue
+++ src/components/menus/NavTop.vue
@@ -66,12 +66,12 @@ onMounted(() => {
             <div>
               <button
                 type="button"
-                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                class="flex text-sm rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                 aria-expanded="false"
                 data-dropdown-toggle="dropdown-user"
               >
                 <span class="sr-only">Open user menu</span>
-                <img class="w-8 h-8 rounded-full" src="../../assets/logo.svg" alt="user photo" />
+                <img class="w-8 h-8 rounded-full" src="/logo.svg" alt="user photo" />
               </button>
             </div>
             <div
--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -54,12 +54,8 @@ async function onSubmit() {
         href="#"
         class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
       >
-        <img
-          class="w-8 h-8 mr-2"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-          alt="logo"
-        />
-        Wings
+        <img class="w-20 h-20 mr-2" src="/logo.svg" alt="Wings" />
+        Wings Deliver
       </a>
       <div
         class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"

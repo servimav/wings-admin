b0b37ca0e7b2f56ee548fd3b31a295ca50c5d754
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-21 09:40:31 -0400

    format document

--- src/helpers/mix.ts
+++ src/helpers/mix.ts
@@ -4,14 +4,14 @@
  * @returns
  */
 export function useStorage<T = unknown>(key: string) {
-  return {
-    get: () => {
-      const data = localStorage.getItem(`${window.location.host}/${key}`)
-      return data ? (JSON.parse(data) as T) : null
-    },
-    set: (data: T) => localStorage.setItem(`${window.location.host}/${key}`, JSON.stringify(data)),
-    remove: () => localStorage.removeItem(`${window.location.host}/${key}`)
-  }
+	return {
+		get: () => {
+			const data = localStorage.getItem(`${window.location.host}/${key}`)
+			return data ? (JSON.parse(data) as T) : null
+		},
+		set: (data: T) => localStorage.setItem(`${window.location.host}/${key}`, JSON.stringify(data)),
+		remove: () => localStorage.removeItem(`${window.location.host}/${key}`)
+	}
 }
 
 /**
@@ -20,20 +20,29 @@ export function useStorage<T = unknown>(key: string) {
  * @returns
  */
 export function toCurrency(val: number) {
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  })
+	const formatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD'
+	})
 
-  return formatter.format(val)
+	return formatter.format(val)
 }
 
 /**
  * scrollTop
  */
 export function scrollTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
-  })
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	})
+}
+
+/**
+ * defaultImage
+ * @returns
+ */
+export function setDefaultImage(event: Event) {
+	console.log({ event });
+	(event.target as HTMLImageElement).src = "/images/default.png";
 }
--- src/views/dashboard/OffersView.vue
+++ src/views/dashboard/OffersView.vue
@@ -32,22 +32,22 @@ const $shop = useShopStore()
 const deleteModalId = 'store-delete-modal'
 const loading = computed(() => $app.loading)
 const offers = computed<ShopOffer[] | undefined>(() => {
-  if (storeId.value) {
-    // Check if store exists
-    const store = $shop.stores.find((s) => s.id === storeId.value)
-    return store && store.offers ? store.offers : []
-  }
-  return []
+	if (storeId.value) {
+		// Check if store exists
+		const store = $shop.stores.find((s) => s.id === storeId.value)
+		return store && store.offers ? store.offers : []
+	}
+	return []
 })
 
 const showOfferForm = ref(false)
 const showStoreForm = ref(false)
 
 const store = computed(() => {
-  if (storeId.value) {
-    return $shop.stores.find((s) => s.id === storeId.value)
-  }
-  return undefined
+	if (storeId.value) {
+		return $shop.stores.find((s) => s.id === storeId.value)
+	}
+	return undefined
 })
 const storeId = ref<number>()
 /**
@@ -60,23 +60,23 @@ const storeId = ref<number>()
  * Handle on edit store button click
  */
 function onEditStoreClick() {
-  showStoreForm.value = true
+	showStoreForm.value = true
 }
 
 /**
  * getOffers
  */
 async function getOffers() {
-  if ($route.params.storeId) {
-    storeId.value = Number($route.params.storeId)
-    $app.toggleLoading(true)
-    try {
-      await $shop.getStoreOffers(storeId.value)
-    } catch (error) {
-      $app.axiosError(error)
-    }
-    $app.toggleLoading(false)
-  }
+	if ($route.params.storeId) {
+		storeId.value = Number($route.params.storeId)
+		$app.toggleLoading(true)
+		try {
+			await $shop.getStoreOffers(storeId.value)
+		} catch (error) {
+			$app.axiosError(error)
+		}
+		$app.toggleLoading(false)
+	}
 }
 
 /**
@@ -84,44 +84,44 @@ async function getOffers() {
  * @param offerId
  */
 function goToOffer(offerId: number) {
-  $router.push({
-    name: ROUTE_NAME.OFFER,
-    params: {
-      storeId: storeId.value,
-      offerId
-    }
-  })
+	$router.push({
+		name: ROUTE_NAME.OFFER,
+		params: {
+			storeId: storeId.value,
+			offerId
+		}
+	})
 }
 
 /**
  * Handle on OfferForm created event
  */
 function onOfferCreated() {
-  showOfferForm.value = false
-  getOffers()
+	showOfferForm.value = false
+	getOffers()
 }
 
 /**
  * onClickFloatButton
  */
 function onClickFloatButton() {
-  showOfferForm.value = true
+	showOfferForm.value = true
 }
 
 /**
  * onStoreDelete
  */
 async function onStoreDelete() {
-  $app.toggleLoading(false)
-  try {
-    const resp = await $service.shop.store.remove(storeId.value as number)
-    console.log({ deleteStore: resp.data })
-    $app.success('Tienda eliminada')
-    $router.push({ name: ROUTE_NAME.STORES })
-  } catch (error) {
-    $app.axiosError(error)
-  }
-  $app.toggleLoading(true)
+	$app.toggleLoading(false)
+	try {
+		const resp = await $service.shop.store.remove(storeId.value as number)
+		console.log({ deleteStore: resp.data })
+		$app.success('Tienda eliminada')
+		$router.push({ name: ROUTE_NAME.STORES })
+	} catch (error) {
+		$app.axiosError(error)
+	}
+	$app.toggleLoading(true)
 }
 
 /**
@@ -129,19 +129,19 @@ async function onStoreDelete() {
  * @param store
  */
 function onStoreUpdated(store: ShopStore) {
-  // Update store data in local stores
-  const storeIndex = $shop.stores.findIndex((s) => s.id === store.id)
+	// Update store data in local stores
+	const storeIndex = $shop.stores.findIndex((s) => s.id === store.id)
 
-  if (storeIndex >= 0) {
-    // create a copy of store offers
-    const storeOffers = $shop.stores[storeIndex].offers
-    // update store value
-    $shop.stores[storeIndex] = store
-    // Restore store offers
-    $shop.stores[storeIndex].offers = storeOffers
-  }
-  // hide form
-  showStoreForm.value = false
+	if (storeIndex >= 0) {
+		// create a copy of store offers
+		const storeOffers = $shop.stores[storeIndex].offers
+		// update store value
+		$shop.stores[storeIndex] = store
+		// Restore store offers
+		$shop.stores[storeIndex].offers = storeOffers
+	}
+	// hide form
+	showStoreForm.value = false
 }
 
 /**
@@ -151,81 +151,73 @@ function onStoreUpdated(store: ShopStore) {
  */
 
 onBeforeMount(async () => {
-  await getOffers()
+	await getOffers()
 })
 
 onMounted(() => {
-  setTimeout(() => {
-    initModals()
-  }, 1000)
+	setTimeout(() => {
+		initModals()
+	}, 1000)
 })
 </script>
 
 <template>
-  <div class="p-2">
-    <!-- Offer Form -->
-    <template v-if="showOfferForm && store">
-      <OfferForm :store-id="store.id" @created="onOfferCreated" @canceled="onOfferCreated" />
-    </template>
-    <!-- / Offer Form -->
+	<div class="p-2">
+		<!-- Offer Form -->
+		<template v-if="showOfferForm && store">
+			<OfferForm :store-id="store.id" @created="onOfferCreated" @canceled="onOfferCreated" />
+		</template>
+		<!-- / Offer Form -->
 
-    <!-- Store Form -->
-    <div v-else-if="showStoreForm" class="p-4 border rounded-md">
-      <StoreForm :update="store" @updated="onStoreUpdated" />
-    </div>
-    <!-- / Store Form -->
+		<!-- Store Form -->
+		<div v-else-if="showStoreForm" class="p-4 border rounded-md">
+			<StoreForm :update="store" @updated="onStoreUpdated" />
+		</div>
+		<!-- / Store Form -->
 
-    <!-- Store Data -->
-    <template v-else-if="store">
-      <div class="border rounded-md p-2">
-        <div class="space-y-1">
-          <h2 class="text-xl text-center font-bold">{{ store.name }}</h2>
-          <p class="font-semibold" :class="store.available ? 'text-green-700' : 'text-slate-600'">
-            {{ store.available ? 'Abierta' : 'Cerrada' }}
-          </p>
-          <p class="font-semibold">Descripcion</p>
-          <p class="text-sm text-justify">{{ store.description }}</p>
-          <p class="font-semibold">Dirección</p>
-          <p class="text-sm text-justify">{{ store.address }}</p>
-        </div>
-        <div class="mt-4">
-          <button class="btn-primary py-1 px-2" @click="onEditStoreClick">Editar</button>
-          <button
-            class="btn-negative py-1 px-2"
-            :data-modal-target="deleteModalId"
-            :data-modal-toggle="deleteModalId"
-          >
-            Eliminar
-          </button>
-        </div>
-      </div>
+		<!-- Store Data -->
+		<template v-else-if="store">
+			<section class="border rounded-md p-2">
+				<div class="space-y-1">
+					<h2 class="text-xl text-center font-bold">{{ store.name }}</h2>
+					<p class="font-semibold" :class="store.available ? 'text-green-700' : 'text-slate-600'">
+						{{ store.available ? 'Abierta' : 'Cerrada' }}
+					</p>
+					<p class="font-semibold">Descripcion</p>
+					<p class="text-sm text-justify">{{ store.description }}</p>
+					<p class="font-semibold">Dirección</p>
+					<p class="text-sm text-justify">{{ store.address }}</p>
+				</div>
+				<div class="mt-4">
+					<button class="btn-primary py-1 px-2" @click="onEditStoreClick">Editar</button>
+					<button class="btn-negative py-1 px-2" :data-modal-target="deleteModalId" :data-modal-toggle="deleteModalId">
+						Eliminar
+					</button>
+				</div>
+			</section>
 
-      <div v-if="offers && offers.length" class="mt-4">
-        <h3 class="text-xl text-center">Productos</h3>
+			<div v-if="offers && offers.length" class="mt-4">
+				<h3 class="text-xl text-center">Productos</h3>
 
-        <div class="mt-2 grid grid-cols-2 gap-2">
-          <OfferWidget
-            v-for="(offer, offerKey) in offers"
-            :key="`offer-${offer.id}-${offerKey}`"
-            :data="offer"
-            @click="() => goToOffer(offer.id)"
-          />
-        </div>
-      </div>
-    </template>
-    <!-- / Store Form -->
+				<div class="mt-2 grid grid-cols-2 gap-2">
+					<OfferWidget v-for="(offer, offerKey) in offers" :key="`offer-${offer.id}-${offerKey}`" :data="offer"
+						@click="() => goToOffer(offer.id)" />
+				</div>
+			</div>
+		</template>
+		<!-- / Store Form -->
 
-    <!-- Loading -->
-    <div v-else-if="loading">
-      <div class="grid grid-cols-2 gap-2">
-        <OfferSkeleton :repeat="4" />
-      </div>
-    </div>
-    <!-- / Loading -->
+		<!-- Loading -->
+		<div v-else-if="loading">
+			<div class="grid grid-cols-2 gap-2">
+				<OfferSkeleton :repeat="4" />
+			</div>
+		</div>
+		<!-- / Loading -->
 
-    <div v-else>Sin Datos que mostrar</div>
-  </div>
+		<div v-else>Sin Datos que mostrar</div>
+	</div>
 
-  <FloatButton @click="onClickFloatButton" />
-  <DeleteModal :id="deleteModalId" :store="store" @delete="onStoreDelete" />
+	<FloatButton @click="onClickFloatButton" />
+	<DeleteModal :id="deleteModalId" :store="store" @delete="onStoreDelete" />
 </template>

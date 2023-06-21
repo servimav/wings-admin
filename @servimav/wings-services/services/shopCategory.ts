c663dd9bd49eb95b25aada90af20c350015551ff
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-06-21 13:04:14 -0400

    add get offers by category endpoint

--- @servimav/wings-services/services/shopCategory.ts
+++ @servimav/wings-services/services/shopCategory.ts
@@ -1,17 +1,20 @@
 import type { AxiosInstance } from 'axios'
 import { generateCrudWithoutPaginate } from '../crud'
+import type { PaginatedData } from '../types'
 
 export default function init(api: AxiosInstance) {
-	const baseUrl = '/shop/categories'
+  const baseUrl = '/shop/categories'
 
-	const crud = generateCrudWithoutPaginate<ShopCategory, ShopCategoryCreate>({
-		api,
-		baseUrl
-	})
+  const crud = generateCrudWithoutPaginate<ShopCategory, ShopCategoryCreate>({
+    api,
+    baseUrl
+  })
 
-	return {
-		...crud
-	}
+  return {
+    ...crud,
+    offers: (id: number, params: { currency?: string }) =>
+      api.get<PaginatedData<ShopCategory>>(`${baseUrl}/${id}/offers`, { params })
+  }
 }
 
 /**
@@ -21,14 +24,14 @@ export default function init(api: AxiosInstance) {
  */
 
 export interface ShopCategory {
-	id: number
-	name: string
-	image?: string
-	parent?: ShopCategory
+  id: number
+  name: string
+  image?: string
+  parent?: ShopCategory
 }
 
 export interface ShopCategoryCreate {
-	name: string
-	image?: string
-	parent_id?: number
+  name: string
+  image?: string
+  parent_id?: number
 }

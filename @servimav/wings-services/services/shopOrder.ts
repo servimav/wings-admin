4f598357e90d02f4b2edb698cb833002456d8628
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-07-14 09:04:16 -0400

    incomming field in order items

--- @servimav/wings-services/services/shopOrder.ts
+++ @servimav/wings-services/services/shopOrder.ts
@@ -7,24 +7,24 @@ import { generateCrud } from '../crud'
 import type { PaginatedData } from '../types'
 
 export default function init(api: AxiosInstance) {
-	const baseURL = '/shop/orders'
+  const baseURL = '/shop/orders'
 
-	const crud = generateCrud<ShopOrder, ShopOrderCreate, ShopOrderUpdate>({
-		api,
-		baseURL
-	})
+  const crud = generateCrud<ShopOrder, ShopOrderCreate, ShopOrderUpdate>({
+    api,
+    baseURL
+  })
 
-	return {
-		...crud,
-		filter: (params: ShopOrderFilter) =>
-			api.get<PaginatedData<ShopOrder>>(`${baseURL}/filter`, { params }),
-		mine: (params: Omit<ShopOrderFilter, 'customer_id' | 'location_id'>) =>
-			api.get<PaginatedData<ShopOrder>>(`${baseURL}/mine`, { params }),
-		show: (id: number, params: { currency: 'CUP' }) =>
-			api.get<ShopOrder>(`${baseURL}/${id}`, { params }),
-		showClient: (id: number, params: { currency: 'CUP' }) =>
-			api.get<ShopOrder>(`${baseURL}/${id}/client`, { params })
-	}
+  return {
+    ...crud,
+    filter: (params: ShopOrderFilter) =>
+      api.get<PaginatedData<ShopOrder>>(`${baseURL}/filter`, { params }),
+    mine: (params: Omit<ShopOrderFilter, 'customer_id' | 'location_id'>) =>
+      api.get<PaginatedData<ShopOrder>>(`${baseURL}/mine`, { params }),
+    show: (id: number, params: { currency: 'CUP' }) =>
+      api.get<ShopOrder>(`${baseURL}/${id}`, { params }),
+    showClient: (id: number, params: { currency: 'CUP' }) =>
+      api.get<ShopOrder>(`${baseURL}/${id}/client`, { params })
+  }
 }
 
 /**
@@ -37,70 +37,71 @@ export default function init(api: AxiosInstance) {
  * ShopOrder
  */
 export interface ShopOrder {
-	id: number
-	customer: User
-	delivery_status: STATUS
-	delivery_details: DeliveryDetails
-	delivery_price: number
-	delivery_date?: string
-	offers_price: number
-	service_price: number
-	items: OrderItem[]
-	payment_type: PaymentType
+  id: number
+  customer: User
+  delivery_status: STATUS
+  delivery_details: DeliveryDetails
+  delivery_price: number
+  delivery_date?: string
+  offers_price: number
+  service_price: number
+  items: OrderItem[]
+  payment_type: PaymentType
 }
 
 /**
  * ShopOrderCreate
  */
 export interface ShopOrderCreate {
-	items: Array<{ id: number; qty: number }>
-	delivery_details: DeliveryDetails
-	currency?: CurrencyCode
-	message?: string
-	payment_type: PaymentType
+  items: Array<{ id: number; qty: number }>
+  delivery_details: DeliveryDetails
+  currency?: CurrencyCode
+  message?: string
+  payment_type: PaymentType
 }
 
 /**
  * ShopOrderUpdate
  */
 export interface ShopOrderUpdate {
-	currency?: CurrencyCode
-	delivery_status?: STATUS
-	delivery_date?: string
+  currency?: CurrencyCode
+  delivery_status?: STATUS
+  delivery_date?: string
 }
 
 /**
  * ShopOrderFilter
  */
 export interface ShopOrderFilter {
-	status?: STATUS
-	currency?: CurrencyCode
-	location_id?: number
-	customer_id?: number
-	page?: number
+  status?: STATUS
+  currency?: CurrencyCode
+  location_id?: number
+  customer_id?: number
+  page?: number
 }
 
 /**
  * OrderOffer
  */
 export interface OrderItem {
-	id: number
-	qty: number
-	name: string
-	image: string
-	price: number
-	offer?: ShopOffer
+  id: number
+  qty: number
+  name: string
+  image: string
+  price: number
+  offer?: ShopOffer
+  incomming?: boolean
 }
 
 /**
  * DeliveryDetails
  */
 export interface DeliveryDetails {
-	name: string
-	contact: string
-	address: string
-	coords?: GeoCoords
-	location_id: number
+  name: string
+  contact: string
+  address: string
+  coords?: GeoCoords
+  location_id: number
 }
 
 export type PaymentType = 'CASH_TOTAL' | 'TRANSFER_PARTIAL' | 'TRANSFER_TOTAL'

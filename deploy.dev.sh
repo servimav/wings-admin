d85a4fcf03c82c93b9e1ccceac876619debfe23c
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-07-17 01:27:23 -0400

    deploy scripts

---
+++ deploy.dev.sh
@@ -0,0 +1,35 @@
+sshCommands="$(cat <<END_HEREDOC
+# Go To dir
+cd /var/www/servimav.com/wings/admin/
+
+# Pull
+git checkout dev
+git pull
+
+# copy env
+cp .env.dev .env
+
+# Install api server dependencies.
+source ~/.nvm/nvm.sh
+
+echo "Switching to appropriate node.js version using nvm"
+if ! nvm use default ; then
+  echo "Error using nvm to switch node.js version."
+  exit 14
+fi
+
+echo "Installing server api dependencies"
+if ! yarn ; then
+  echo "Error installing server api dependencies."
+  exit 15
+fi
+
+echo "Installing building app"
+if ! yarn build ; then
+  echo "Error building app."
+  exit 16
+fi
+END_HEREDOC
+)"
+
+ssh adricq@vps1.servimav.com -t "$sshCommands"

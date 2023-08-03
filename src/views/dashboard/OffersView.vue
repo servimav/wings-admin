9daaae23c0caec9a9d11bff08559a2e8a66ac079
Adrian Capote Quintana <adriancapote95@gmail.com>
2023-08-02 21:44:58 -0400

    ignore type check

--- src/views/dashboard/OffersView.vue
+++ src/views/dashboard/OffersView.vue
@@ -14,7 +14,7 @@ import { onBeforeRouteUpdate, useRoute, useRouter } from 'vue-router'
 const BaseModal = defineAsyncComponent(() => import('@/components/modals/BaseModal.vue'))
 const FloatButton = defineAsyncComponent(() => import('@/components/buttons/FloatButton.vue'))
 const FloatButtonItem = defineAsyncComponent(
-  () => import('@/components/buttons/FloatButtonItem.vue')
+	() => import('@/components/buttons/FloatButtonItem.vue')
 )
 const IconPlus = defineAsyncComponent(() => import('@/components/icons/PlusIcon.vue'))
 const IconSearch = defineAsyncComponent(() => import('@/components/icons/MangifyGlass.vue'))
@@ -43,11 +43,11 @@ const loading = computed(() => $app.loading)
 const offers = ref<ShopOffer[]>([])
 const offersCurrentPage = ref<number>()
 const scrollHandler = () => {
-  const scrollable = document.documentElement.scrollHeight - window.innerHeight
-  const scrolled = window.scrollY
-  if (scrollable - scrolled <= 1) {
-    filterOffers()
-  }
+	const scrollable = document.documentElement.scrollHeight - window.innerHeight
+	const scrolled = window.scrollY
+	if (scrollable - scrolled <= 1) {
+		filterOffers()
+	}
 }
 const showFloatMenu = ref(false)
 const showFilterModal = ref(false)
@@ -65,30 +65,30 @@ const store = ref<ShopStore>()
  * @param params
  */
 async function filterOffers() {
-  if (loading.value) return
-  $app.toggleLoading(true)
-  try {
-    console.log({ filter: filter.value })
-    const resp = (
-      await $service.shop.offer.filterAdvanced({
-        ...filter.value,
-        sort: 'id',
-        page: offersCurrentPage.value ? offersCurrentPage.value + 1 : undefined
-      })
-    ).data
-    offersCurrentPage.value = resp.meta.current_page
-    if (resp.data.length) {
-      // if is first search overwrite offers
-      offers.value.push(...resp.data)
-    } else {
-      // Stop event listner
-      window.removeEventListener('scroll', scrollHandler)
-    }
-  } catch (error) {
-    $app.axiosError(error)
-    window.removeEventListener('scroll', scrollHandler)
-  }
-  $app.toggleLoading(false)
+	if (loading.value) return
+	$app.toggleLoading(true)
+	try {
+		console.log({ filter: filter.value })
+		const resp = (
+			await $service.shop.offer.filterAdvanced({
+				...filter.value,
+				sort: 'id',
+				page: offersCurrentPage.value ? offersCurrentPage.value + 1 : undefined
+			})
+		).data
+		offersCurrentPage.value = resp.meta.current_page
+		if (resp.data.length) {
+			// if is first search overwrite offers
+			offers.value.push(...resp.data)
+		} else {
+			// Stop event listner
+			window.removeEventListener('scroll', scrollHandler)
+		}
+	} catch (error) {
+		$app.axiosError(error)
+		window.removeEventListener('scroll', scrollHandler)
+	}
+	$app.toggleLoading(false)
 }
 
 /**
@@ -96,32 +96,32 @@ async function filterOffers() {
  * @param id
  */
 async function getStore(storeId?: number) {
-  if (storeId) {
-    try {
-      const resp = (await $service.shop.store.show(storeId)).data
-      // update store
-      const index = $shop.stores.findIndex((s) => s.id === storeId)
-      if (index >= 0) {
-        $shop.stores[index] = resp
-      }
-      // set current store
-      store.value = resp
-    } catch (error) {
-      $app.axiosError(error)
-    }
-  }
+	if (storeId) {
+		try {
+			const resp = (await $service.shop.store.show(storeId)).data
+			// update store
+			const index = $shop.stores.findIndex((s) => s.id === storeId)
+			if (index >= 0) {
+				$shop.stores[index] = resp
+			}
+			// set current store
+			store.value = resp
+		} catch (error) {
+			$app.axiosError(error)
+		}
+	}
 }
 /**
  * goToOffer
  * @param offer
  */
 function goToOffer(offer: ShopOffer) {
-  $router.push({
-    name: ROUTE_NAME.OFFER,
-    params: {
-      offerId: offer.id
-    }
-  })
+	$router.push({
+		name: ROUTE_NAME.OFFER,
+		params: {
+			offerId: offer.id
+		}
+	})
 }
 
 /**
@@ -129,13 +129,13 @@ function goToOffer(offer: ShopOffer) {
  * @param offer
  */
 function onCreateOffer(offer: ShopOffer) {
-  showNewOfferModal.value = false
-  $router.push({
-    name: ROUTE_NAME.OFFER,
-    params: {
-      offerId: offer.id
-    }
-  })
+	showNewOfferModal.value = false
+	$router.push({
+		name: ROUTE_NAME.OFFER,
+		params: {
+			offerId: offer.id
+		}
+	})
 }
 
 /**
@@ -143,24 +143,25 @@ function onCreateOffer(offer: ShopOffer) {
  * @param search
  */
 async function onSearchOffer(search: ShopOfferFilter) {
-  // Srcoll to Top
-  scrollTop()
-  // restart listener
-  window.removeEventListener('scroll', scrollHandler)
-  // close modal
-  showFilterModal.value = false
-  // init search params
-  filter.value = search
-  offersCurrentPage.value = undefined
-  offers.value = []
-  // replace route
-  void $router.replace({
-    name: ROUTE_NAME.OFFERS,
-    query: { ...search }
-  })
-  // run query
-  await Promise.all([getStore(filter.value?.store_id), filterOffers()])
-  window.addEventListener('scroll', scrollHandler)
+	// Srcoll to Top
+	scrollTop()
+	// restart listener
+	window.removeEventListener('scroll', scrollHandler)
+	// close modal
+	showFilterModal.value = false
+	// init search params
+	filter.value = search
+	offersCurrentPage.value = undefined
+	offers.value = []
+	// replace route
+	void $router.replace({
+		name: ROUTE_NAME.OFFERS,
+		// @ts-ignore
+		query: { ...search }
+	})
+	// run query
+	await Promise.all([getStore(filter.value?.store_id), filterOffers()])
+	window.addEventListener('scroll', scrollHandler)
 }
 
 /**
@@ -172,138 +173,118 @@ async function onSearchOffer(search: ShopOfferFilter) {
  * onBeforeMount
  */
 onBeforeMount(() => {
-  // get query from route
-  const filterQuery: ShopOfferFilter = $route.query
-  // Check if route is STORE_OFFERS or OFFERS
-  const routeName = $route.name
-  if (routeName === ROUTE_NAME.STORE_OFFERS) {
-    const storeId = Number($route.params.storeId)
-    filterQuery.store_id = storeId
-  }
+	// get query from route
+	const filterQuery: ShopOfferFilter = $route.query
+	// Check if route is STORE_OFFERS or OFFERS
+	const routeName = $route.name
+	if (routeName === ROUTE_NAME.STORE_OFFERS) {
+		const storeId = Number($route.params.storeId)
+		filterQuery.store_id = storeId
+	}
 
-  // init search params
-  filter.value = filterQuery
-  offersCurrentPage.value = undefined
-  offers.value = []
+	// init search params
+	filter.value = filterQuery
+	offersCurrentPage.value = undefined
+	offers.value = []
 })
 
 /**
  * onMounted
  */
 onMounted(async () => {
-  scrollTop()
-  await Promise.all([getStore(filter.value?.store_id), filterOffers()])
-  // register scroll event listener
-  window.addEventListener('scroll', scrollHandler)
+	scrollTop()
+	await Promise.all([getStore(filter.value?.store_id), filterOffers()])
+	// register scroll event listener
+	window.addEventListener('scroll', scrollHandler)
 })
 
 /**
  * onBeforeUnmount
  */
 onBeforeUnmount(() => {
-  // unregister scroll listener
-  window.removeEventListener('scroll', scrollHandler)
+	// unregister scroll listener
+	window.removeEventListener('scroll', scrollHandler)
 })
 
 /**
  * onBeforeRouteUpdate
  */
 onBeforeRouteUpdate(async ($to, $from, $next) => {
-  // get query from route
-  const filterQuery: ShopOfferFilter = $to.query
-  // Check if route is STORE_OFFERS or OFFERS
-  const routeName = $to.name
-  if (routeName === ROUTE_NAME.STORE_OFFERS) {
-    const storeId = Number($to.params.storeId)
-    filterQuery.store_id = storeId
-  }
-  // Init search params
-  filter.value = filterQuery
-  offersCurrentPage.value = undefined
-  offers.value = []
+	// get query from route
+	const filterQuery: ShopOfferFilter = $to.query
+	// Check if route is STORE_OFFERS or OFFERS
+	const routeName = $to.name
+	if (routeName === ROUTE_NAME.STORE_OFFERS) {
+		const storeId = Number($to.params.storeId)
+		filterQuery.store_id = storeId
+	}
+	// Init search params
+	filter.value = filterQuery
+	offersCurrentPage.value = undefined
+	offers.value = []
 
-  await Promise.all([getStore(filter.value?.store_id), filterOffers()])
+	await Promise.all([getStore(filter.value?.store_id), filterOffers()])
 
-  $next($to)
+	$next($to)
 })
 </script>
 
 <template>
-  <section class="p-2">
-    <!-- Offers Content -->
-    <div class="grid grid-cols-2 gap-2" v-if="offers.length">
-      <OfferWidget
-        v-for="(offer, offerKey) in offers"
-        :key="`offer-${offer.id}-${offerKey}`"
-        :data="offer"
-        @click="() => goToOffer(offer)"
-      />
+	<section class="p-2">
+		<!-- Offers Content -->
+		<div class="grid grid-cols-2 gap-2" v-if="offers.length">
+			<OfferWidget v-for="(offer, offerKey) in offers" :key="`offer-${offer.id}-${offerKey}`" :data="offer"
+				@click="() => goToOffer(offer)" />
 
-      <OfferSkeleton :repeat="4" v-if="loading" />
-    </div>
-    <!-- / Offers Content -->
+			<OfferSkeleton :repeat="4" v-if="loading" />
+		</div>
+		<!-- / Offers Content -->
 
-    <!-- Loading -->
-    <div class="grid grid-cols-2 gap-2" v-else-if="loading">
-      <OfferSkeleton :repeat="8" />
-    </div>
-    <!-- / Loading -->
+		<!-- Loading -->
+		<div class="grid grid-cols-2 gap-2" v-else-if="loading">
+			<OfferSkeleton :repeat="8" />
+		</div>
+		<!-- / Loading -->
 
-    <!-- No content -->
-    <div class="flex min-h-[32rem] items-center justify-center" v-else>
-      <div class="font-gray-700">No hay contenido</div>
-    </div>
-    <!-- / No content -->
-  </section>
+		<!-- No content -->
+		<div class="flex min-h-[32rem] items-center justify-center" v-else>
+			<div class="font-gray-700">No hay contenido</div>
+		</div>
+		<!-- / No content -->
+	</section>
 
-  <div class="fixed bottom-6 left-6 z-10">
-    <FloatButton v-model="showFloatMenu">
-      <FloatButtonItem
-        @click="
-          () => {
-            showFloatMenu = false
-            showNewOfferModal = true
-          }
-        "
-        label="Nuevo"
-        :icon="IconPlus"
-        id="plus-float"
-      />
-      <FloatButtonItem
-        @click="
-          () => {
-            showFloatMenu = false
-            showFilterModal = true
-          }
-        "
-        label="Buscar"
-        :icon="IconSearch"
-        id="search-float"
-      />
-    </FloatButton>
-  </div>
+	<div class="fixed bottom-6 left-6 z-10">
+		<FloatButton v-model="showFloatMenu">
+			<FloatButtonItem @click="() => {
+					showFloatMenu = false
+					showNewOfferModal = true
+				}
+				" label="Nuevo" :icon="IconPlus" id="plus-float" />
+			<FloatButtonItem @click="() => {
+					showFloatMenu = false
+					showFilterModal = true
+				}
+				" label="Buscar" :icon="IconSearch" id="search-float" />
+		</FloatButton>
+	</div>
 
-  <!-- Search Modal -->
-  <BaseModal v-if="showFilterModal" @close="() => (showFilterModal = false)">
-    <div class="flex items-center justify-center">
-      <div class="z-30 w-full rounded-xl border bg-white p-4">
-        <SearchForm
-          :filter="filter"
-          @close="() => (showFilterModal = false)"
-          @search="onSearchOffer"
-        />
-      </div>
-    </div>
-  </BaseModal>
-  <!-- / Search Modal -->
+	<!-- Search Modal -->
+	<BaseModal v-if="showFilterModal" @close="() => (showFilterModal = false)">
+		<div class="flex items-center justify-center">
+			<div class="z-30 w-full rounded-xl border bg-white p-4">
+				<SearchForm :filter="filter" @close="() => (showFilterModal = false)" @search="onSearchOffer" />
+			</div>
+		</div>
+	</BaseModal>
+	<!-- / Search Modal -->
 
-  <!-- New offer Modal -->
-  <BaseModal v-if="showNewOfferModal" @close="() => (showNewOfferModal = false)">
-    <div class="flex items-center justify-center">
-      <div class="z-30 w-full rounded-xl border bg-white">
-        <OfferForm @canceled="() => (showNewOfferModal = false)" @created="onCreateOffer" />
-      </div>
-    </div>
-  </BaseModal>
-  <!-- / New offer Modal -->
+	<!-- New offer Modal -->
+	<BaseModal v-if="showNewOfferModal" @close="() => (showNewOfferModal = false)">
+		<div class="flex items-center justify-center">
+			<div class="z-30 w-full rounded-xl border bg-white">
+				<OfferForm @canceled="() => (showNewOfferModal = false)" @created="onCreateOffer" />
+			</div>
+		</div>
+	</BaseModal>
+	<!-- / New offer Modal -->
 </template>
